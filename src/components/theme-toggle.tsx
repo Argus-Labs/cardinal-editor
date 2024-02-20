bfe7b04475114b8bf40ff75c0aa754ee2aa12e16
Ryan Martin <rmrt1n@users.noreply.github.com>
2024-02-21 02:23:50 +0700

    feat: sidebar layout

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -2,8 +2,13 @@ import { Moon, Sun } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import { useTheme } from "@/components/theme-provider"
+import { cn } from "@/lib/utils"
 
-export function ThemeToggle() {
+interface ThemeToggleProps {
+  className?: string
+}
+
+export function ThemeToggle({ className }: ThemeToggleProps) {
   const { theme, setTheme } = useTheme()
 
   const toggleTheme = () => {
@@ -14,7 +19,7 @@ export function ThemeToggle() {
     <Button
       variant="outline"
       size="icon"
-      className="flex-shrink-0"
+      className={cn("flex-shrink-0", className)}
       onClick={toggleTheme}
     >
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react'
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 
-import logo from '@/assets/logo.svg'
+import logo from '@/assets/world.svg'
 import { Input } from '@/components/ui/input'
 import { ThemeToggle } from '@/components/theme-toggle'
 
@@ -16,25 +16,46 @@ const TanStackRouterDevtools =
       })),
     )
 
+// TODO: autofill from cardinal, will probably need custom components for queries & messages
+const sidebarNav = [
+  {
+    title: 'Queries',
+    items: []
+  },
+  {
+    title: 'Messages',
+    items: []
+  },
+]
+
 export const Route = createRootRoute({
   component: () => (
     <>
-      <header className="border">
-        <nav className="flex items-center justify-between lg:container p-2">
+      <header className="border-b">
+        <nav className="flex items-center justify-between px-3 h-12">
           <Link to="/">
             <img src={logo} width={32} height={32} />
           </Link>
           {/* TODO: connect with running cardinal instance */}
           <div className="flex items-center gap-2">
-            <ThemeToggle />
+            <label htmlFor="host" className="flex-shrink-0 text-xs text-muted-foreground">Cardinal URL</label>
+            <Input id="host" placeholder="localhost:3333" value="localhost:3333" className="h-8" />
             <div className="size-2 rounded-full bg-green-500 flex-shrink-0" />
-            <label htmlFor="host" className="flex-shrink-0 text-sm">Cardinal URL</label>
-            <Input id="host" placeholder="localhost:3333" value="localhost:3333" />
           </div>
         </nav>
       </header>
-      <main className="lg:container p-2">
-        <Outlet />
+      <main className="flex min-h-[calc(100%-3rem-1px)]">
+        <aside className="flex flex-col justify-between px-3 py-2 min-w-64 border-r text-sm">
+          <div>
+            {sidebarNav.map((item) => (
+              <p className="font-bold px-2 py-1">{item.title}</p>
+            ))}
+          </div>
+          <ThemeToggle className="self-end" />
+        </aside>
+        <div className="bg-muted w-full px-3 py-2">
+          <Outlet />
+        </div>
       </main>
       <Suspense>
         <TanStackRouterDevtools />
