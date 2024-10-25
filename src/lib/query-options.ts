e965b7347526d293d5243c394444325ce8e0eba7
Ryan Martin <rmrt1n@users.noreply.github.com>
2024-10-25 17:01:14 +0700

    fix: parse int radix

--- src/lib/query-options.ts
+++ src/lib/query-options.ts
@@ -260,7 +260,7 @@ function getDurationMicroSeconds(lookback: string) {
   }
   const unit = lookback[lookback.length - 1]
   const unitToSeconds = lookupSeconds[unit]
-  const seconds = parseInt(lookback.slice(0, -1))
+  const seconds = parseInt(lookback.slice(0, -1), 10)
   return seconds * unitToSeconds * 1_000_000
 }
 
